106288f8c379c892438bd642116498789c77439f
Matiix310 <Matiix310@users.noreply.github.com>
2021-07-31 19:46:51 +0200

    Edit style

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-console.log("Serveur lancé avec succes sur le port 8888");
+console.log("Serveur lancé avec succes sur le port 8000");
 
 module.exports = app;
